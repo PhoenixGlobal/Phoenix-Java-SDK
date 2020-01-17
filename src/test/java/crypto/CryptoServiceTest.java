47551acc9a7403245d49b4f0a34390a0d4954c53
Artem Eger <ae@atr0x.com>
2020-01-17 10:19:17 +0100

    crypto update

--- src/test/java/crypto/CryptoServiceTest.java
+++ src/test/java/crypto/CryptoServiceTest.java
@@ -1,82 +1,92 @@
 package crypto;
 
-import org.junit.After;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.ByteArrayOutputStream;
 import java.security.KeyPair;
+import java.security.KeyStore;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.interfaces.ECPrivateKey;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 import static org.junit.Assert.assertArrayEquals;
 
 public class CryptoServiceTest {
 
     private CryptoService classUnderTest = new CryptoService();
-    private static String keyStoreName = "signKeyStore";
-    private static String keyName = "wallet1";
-    private static String password = "mypassword";
+    private String rawKey = "4b92e7309619a293cd2f54fa57bd3d1754c88c77de407859c558299e6892e9a0";
+    private String wif = "KykciLABWjWUShVgm3dHhAMSe4tEWi863Tk6muHosEnFnJ4THi5B";
+    private String address = "APEsKZ2pYmxteuQ226VhGwb2TJKKSLdTGD4";
+    private String mnemonic = "shell scatter method illegal area bid law genius found maze hope negative kit soldier promote various power true reward success own decrease retire raven";
+    private String mnemonicPriv = "c5780e30b880b42c1f7b075c3131b5c9f7af9d2b0f8ca95d2ae2ec29e0726dfd";
+    private String password = "mypassword";
 
     @Test
-    public void generateSaveLoadKeyStore() throws Exception {
-        classUnderTest.generateKeystore(keyStoreName, keyName, password);
-        assertTrue(Files.exists(Paths.get(keyStoreName + ".ubr")));
-        classUnderTest.loadKeyPairFromKeyStore(keyStoreName + ".ubr", keyName, password);
+    public void generateKeyStore() throws Exception {
+        final KeyStore keyStore = classUnderTest.generateKeystore(password);
+        assertNotNull(keyStore.getKey(CryptoService.KEY_NAME, password.toCharArray()));
     }
 
     @Test(expected = Exception.class)
-    public void LoadKeyStoreWithWrongKeystore() throws Exception {
-        classUnderTest.generateKeystore(keyStoreName, keyName, password);
-        assertTrue(Files.exists(Paths.get(keyStoreName + ".ubr")));
-        classUnderTest.loadKeyPairFromKeyStore("keystore.ubr",keyName, password);
-    }
-
-@Test(expected = Exception.class)
-    public void LoadKeyStoreWithWrongKeyname() throws Exception {
-        classUnderTest.generateKeystore(keyStoreName, keyName, password);
-        assertTrue(Files.exists(Paths.get(keyStoreName + ".ubr")));
-        classUnderTest.loadKeyPairFromKeyStore(keyStoreName + ".ubr", "wallet2", password);
-    }
-
-@Test(expected = Exception.class)
     public void LoadKeyStoreWithWrongPass() throws Exception {
-        classUnderTest.generateKeystore(keyStoreName, keyName, password);
-        assertTrue(Files.exists(Paths.get(keyStoreName + ".ubr")));
-        classUnderTest.loadKeyPairFromKeyStore(keyStoreName + ".ubr", keyName,"this");
+        final KeyStore keyStore = classUnderTest.generateKeystore(password);
+        try(ByteArrayOutputStream out = new ByteArrayOutputStream()){
+            keyStore.store(out, password.toCharArray());
+            classUnderTest.loadKeyPairFromKeyStore(out.toByteArray(), "wrongpass", CryptoService.KEY_NAME);
+        }
+    }
+
+    @Test
+    public void LoadKeyStoreWithRightPass() throws Exception {
+        final KeyStore keyStore = classUnderTest.generateKeystore(password);
+        try(ByteArrayOutputStream out = new ByteArrayOutputStream()){
+            keyStore.store(out, password.toCharArray());
+            final KeyPair pair = classUnderTest.loadKeyPairFromKeyStore(out.toByteArray(), password, CryptoService.KEY_NAME);
+            assertEquals(pair.getPrivate().hashCode(), keyStore.getKey(CryptoService.KEY_NAME, password.toCharArray()).hashCode());
+        }
     }
 
     @Test
     public void createSignatureAndVerifyTest() throws Exception {
-        classUnderTest.generateKeystore(keyStoreName, keyName, password);
-        assertTrue(Files.exists(Paths.get(keyStoreName  + ".ubr")));
-        KeyPair keyPair = classUnderTest.loadKeyPairFromKeyStore(keyStoreName + ".ubr", keyName, password);
-        byte[] data = new byte[20];
-        new SecureRandom().nextBytes(data);
-        byte[] signatureBytes = classUnderTest.getSignature(keyPair.getPrivate(), data);
-        assertTrue(classUnderTest.verifySignature(keyPair.getPublic(), data, signatureBytes));
+        final KeyStore keyStore = classUnderTest.generateKeystore(password);
+        try(ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            keyStore.store(out, password.toCharArray());
+            KeyPair keyPair = classUnderTest.loadKeyPairFromKeyStore(out.toByteArray(), password, CryptoService.KEY_NAME);
+            byte[] data = new byte[20];
+            new SecureRandom().nextBytes(data);
+            byte[] signatureBytes = classUnderTest.getSignature(keyPair.getPrivate(), data);
+            assertTrue(classUnderTest.verifySignature(keyPair.getPublic(), data, signatureBytes));
+        }
     }
 
     @Test
     public void encodedToPublicKeyTest() throws Exception {
-        classUnderTest.generateKeystore(keyStoreName, keyName, password);
-        assertTrue(Files.exists(Paths.get(keyStoreName + ".ubr")));
-        final KeyPair keyPair = classUnderTest.loadKeyPairFromKeyStore(keyStoreName + ".ubr", keyName, password);
-        final PublicKey publicKey = keyPair.getPublic();
-        byte [] encodedBytes = publicKey.getEncoded();
-        final PublicKey publicKey2 = classUnderTest.encodedToPublicKey(encodedBytes);
-        assertArrayEquals(publicKey.getEncoded(), publicKey2.getEncoded());
-        assertEquals(publicKey.hashCode(), publicKey2.hashCode());
+        final KeyStore keyStore = classUnderTest.generateKeystore(password);
+        try(ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            keyStore.store(out, password.toCharArray());
+            final KeyPair keyPair = classUnderTest.loadKeyPairFromKeyStore(out.toByteArray(), password, CryptoService.KEY_NAME);
+            final PublicKey publicKey = keyPair.getPublic();
+            byte[] encodedBytes = publicKey.getEncoded();
+            final PublicKey publicKey2 = classUnderTest.encodedToPublicKey(encodedBytes);
+            assertArrayEquals(publicKey.getEncoded(), publicKey2.getEncoded());
+            assertEquals(publicKey.hashCode(), publicKey2.hashCode());
+        }
     }
 
-    @After
-    public void cleanUp() throws IOException {
-        if(Files.exists(Paths.get(keyStoreName + ".ubr"))){
-            Files.delete(Paths.get(keyStoreName + ".ubr"));
-        }
+    @Test
+    public void generateKeyStoreFromWifTest() throws Exception {
+        final KeyStore keyStore = classUnderTest.generateKeyStoreFromWif(password, wif);
+        final ECPrivateKey privateKey = (ECPrivateKey) keyStore.getKey(CryptoService.KEY_NAME, password.toCharArray());
+        assertEquals(address, CPXKey.getPublicAddressCPX(privateKey));
+    }
+
+    @Test
+    public void generateKeyStoreFromMnemonicTest() throws Exception {
+        final KeyStore keyStore = classUnderTest.generateKeyStoreFromMnemonic(password, mnemonic);
+        final ECPrivateKey privateKey = (ECPrivateKey) keyStore.getKey(CryptoService.KEY_NAME, password.toCharArray());
+        final ECPrivateKey mnemonicPrivKey = classUnderTest.getECPrivateKeyFromRawString(mnemonicPriv);
+        assertEquals(mnemonicPrivKey.hashCode(), privateKey.hashCode());
     }
 
 }
